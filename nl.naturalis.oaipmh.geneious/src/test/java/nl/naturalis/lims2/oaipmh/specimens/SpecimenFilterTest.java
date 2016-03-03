fc6a2ed311010eaf0f1c9a8432113811c3a4220b
Ayco Holleman <ayco.holleman@naturalis.nl>
2016-03-03 11:46:06 +0100

    Fixed SpecimenFilterTest

--- nl.naturalis.oaipmh.geneious/src/test/java/nl/naturalis/lims2/oaipmh/specimens/SpecimenFilterTest.java
+++ nl.naturalis.oaipmh.geneious/src/test/java/nl/naturalis/lims2/oaipmh/specimens/SpecimenFilterTest.java
@@ -1,14 +1,24 @@
 package nl.naturalis.lims2.oaipmh.specimens;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+
 import nl.naturalis.lims2.oaipmh.AnnotatedDocument;
-import nl.naturalis.lims2.oaipmh.Document;
-import nl.naturalis.lims2.oaipmh.DocumentNotes;
-import nl.naturalis.lims2.oaipmh.DocumentNotes.Note;
+import nl.naturalis.lims2.oaipmh.MockResultSet;
 
 import org.junit.Test;
 
 public class SpecimenFilterTest {
+
+	@Test()
+	@SuppressWarnings({ "static-method", "resource" })
+	public void testAcceptResultSet() throws SQLException
+	{
+		SpecimenFilter filter = new SpecimenFilter();
+		MockResultSet rs = new MockResultSet();
+		assertTrue("01", filter.accept(rs));
+	}
 
 	@Test()
 	@SuppressWarnings("static-method")
@@ -16,18 +26,6 @@ public class SpecimenFilterTest {
 	{
 		SpecimenFilter filter = new SpecimenFilter();
 		AnnotatedDocument ad = new AnnotatedDocument();
-		assertFalse("01", filter.accept(ad));
-		Document doc = new Document();
-		ad.setDocument(doc);
-		assertFalse("02", filter.accept(ad));
-		DocumentNotes notes = new DocumentNotes();
-		doc.setNotes(notes);
-		assertFalse("03", filter.accept(ad));
-		notes.set(Note.CRSCode_CRS, "");
-		assertFalse("04", filter.accept(ad));
-		notes.set(Note.CRSCode_CRS, "false");
-		assertFalse("05", filter.accept(ad));
-		notes.set(Note.CRSCode_CRS, "true");
-		assertTrue("06", filter.accept(ad));
+		assertTrue("01", filter.accept(ad));
 	}
 }
